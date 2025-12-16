532af3c462be2d70433f4525f9b6ae0a14dd3ce8
PerevoznikovLeonid <perevoznikovleonid@gmail.com>
2025-12-16 21:14:16 +0300

    Delete ReminderApp-main directory

--- ReminderApp-main/headers/ErrorTypes.h
+++
@@ -1,8 +0,0 @@
-#ifndef REMINDERAPP_ERRORTYPES_H
-#define REMINDERAPP_ERRORTYPES_H
-
-enum ErrorType {
-    YearError, MonthError, DayError, HourError, MinutesError
-};
-
-#endif //REMINDERAPP_ERRORTYPES_H
